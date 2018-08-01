6a6fb41f418d9291d2837a669ddf3321f75db236
Maku <hzk.butterfly459@gmail.com>
2018-08-02 01:22:18 +0900

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <p>
+      {{msg}}
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      tag: 'Hello World!'
+    }
+  }
+}
+</script>
+
+<style lang= "scss">
+$font: #4A4A4A;
+
+#TagRow {
+    width: 100%;
+    width: 90px;
+    height: 44px;
+    color: $font;
+}
+</style>
